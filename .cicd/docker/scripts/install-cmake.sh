cc1cbd3f95dcb4c24c0732cc4f1c13a7894e6d11
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-03-05 13:21:44 +0100

    improve docker image for cicd

---
+++ .cicd/docker/scripts/install-cmake.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eux
+apt remove --purge --auto-remove cmake
+
+VERSION=3.22
+BUILD=1
+
+mkdir -p /opt
+wget https://github.com/Kitware/CMake/releases/download/v${VERSION}.${BUILD}/cmake-${VERSION}.${BUILD}-linux-x86_64.tar.gz -O /opt/cmake.tar.gz
+tar -xzvf /opt/cmake.tar.gz -C opt/
+rm /opt/cmake.tar.gz
+
+ln -s /opt/cmake-${VERSION}.${BUILD}-linux-x86_64/bin/* /usr/bin/
