c2475b0967a3f47a4f7ab7a5c45c598886ebef3e
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-03-05 17:44:37 +0100

    fix problem with cicd imge with path

--- .cicd/docker/scripts/install-cmake.sh
+++ .cicd/docker/scripts/install-cmake.sh
@@ -7,7 +7,7 @@ BUILD=1
 
 mkdir -p /opt
 wget https://github.com/Kitware/CMake/releases/download/v${VERSION}.${BUILD}/cmake-${VERSION}.${BUILD}-linux-x86_64.tar.gz -O /opt/cmake.tar.gz
-tar -xzvf /opt/cmake.tar.gz -C opt/
+tar -xzvf /opt/cmake.tar.gz -C /opt/
 rm /opt/cmake.tar.gz
 
 ln -s /opt/cmake-${VERSION}.${BUILD}-linux-x86_64/bin/* /usr/bin/
