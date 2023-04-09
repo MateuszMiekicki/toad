9c08c0fcaba4cc131645ec1ed9435b9cace37b78
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-04-09 21:05:17 +0200

    added tests for unsubscriber procedure

--- packages/communication_protocol/mqtt/broker/tests/unit_tests/SubscriptionManagerTests.cc
+++ packages/communication_protocol/mqtt/broker/tests/unit_tests/SubscriptionManagerTests.cc
@@ -54,3 +54,56 @@ TEST_F(SubscriptionManagerTestFixture, twoDifferentSubscribersSubscribingToSameT
 
     ASSERT_EQ(sut.getNumberActiveSubscribers(), 2);
 }
+
+TEST_F(SubscriptionManagerTestFixture, unsubscribingFromOnlyTopicSubscribedToShouldReduceNumberOfActiveSubscribersByOne)
+{
+    auto subscriberAnna = std::make_shared<Connection>(nullptr);
+    Subscription subscriptionTopicTest{subscriberAnna, "test", {}};
+    sut.subscribe(subscriptionTopicTest);
+
+    ASSERT_EQ(sut.getNumberActiveSubscribers(), 1);
+
+    sut.unsubscribe(subscriptionTopicTest);
+    ASSERT_EQ(sut.getNumberActiveSubscribers(), 0);
+}
+
+TEST_F(SubscriptionManagerTestFixture,
+       unsubscribingFromOneOfTwoSubscribedTopicsShouldNotReduceNumberOfActiveSubscribers)
+{
+    auto subscriberAnna = std::make_shared<Connection>(nullptr);
+    Subscription subscriptionTopicTest{subscriberAnna, "test", {}};
+    sut.subscribe(subscriptionTopicTest);
+    Subscription subscriptionTopicConfig{subscriberAnna, "config", {}};
+    sut.subscribe(subscriptionTopicConfig);
+
+    ASSERT_EQ(sut.getNumberActiveSubscribers(), 1);
+
+    sut.unsubscribe(subscriptionTopicTest);
+    ASSERT_EQ(sut.getNumberActiveSubscribers(), 1);
+}
+
+TEST_F(SubscriptionManagerTestFixture,
+       unsubscribingClientFromTopicToWhichHeHasNotSubscribedShouldNotAffectOtherSubscribedTopics)
+{
+    auto subscriberAnna = std::make_shared<Connection>(nullptr);
+    Subscription subscriptionTopicTest{subscriberAnna, "test", {}};
+    sut.subscribe(subscriptionTopicTest);
+    Subscription subscriptionTopicConfig{subscriberAnna, "config", {}};
+
+    ASSERT_EQ(sut.getNumberActiveSubscribers(), 1);
+
+    sut.unsubscribe(subscriptionTopicConfig);
+    ASSERT_EQ(sut.getNumberActiveSubscribers(), 1);
+}
+
+TEST_F(SubscriptionManagerTestFixture,
+       unsubscribingFromTopicByClientWhoIsNotSubscribedToAnythingShouldNotHaveEffectsOnOtherTopics)
+{
+    auto subscriberAnna = std::make_shared<Connection>(nullptr);
+    Subscription subscriptionTopicTest{subscriberAnna, "test", {}};
+
+    ASSERT_EQ(sut.getNumberActiveSubscribers(), 0);
+
+    sut.unsubscribe(subscriptionTopicTest);
+    ASSERT_EQ(sut.getNumberActiveSubscribers(), 0);
+}
