63da7d60dcdcec4c9b3abf62adfedc454913cf38
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-05-28 20:48:23 +0200

    new version of tcp serwer with mac address

--- services/relay/src/Relay.cc
+++ services/relay/src/Relay.cc
@@ -1,123 +1,151 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
 #include <boost/asio.hpp>
-#include "toad/services/relay/Logger.hh"
 
 using boost::asio::ip::tcp;
 
-class TCPServer
-{
-  public:
-    TCPServer(boost::asio::io_context& io_context, unsigned short port) :
-        acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+class TcpServer {
+public:
+    TcpServer(boost::asio::io_context& io_context, short port)
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
     {
-        startAccept();
+        doAccept();
     }
 
-  private:
-    void startAccept()
+private:
+    void doAccept()
     {
-        auto newConnection = std::make_shared<tcp::socket>(acceptor_.get_executor());
-
-        acceptor_.async_accept(*newConnection,
-                               [this, newConnection](const boost::system::error_code& error)
-                               {
-            if(!error)
+        acceptor_.async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket)
             {
-                handleClient(std::move(newConnection));
+                if (!ec)
+                {
+                    std::make_shared<Session>(std::move(socket), macAddressMap_)->start();
+                }
+
+                doAccept();
+            });
+    }
+
+    class Session : public std::enable_shared_from_this<Session> {
+    public:
+        Session(tcp::socket socket, std::unordered_map<std::string, tcp::socket*>& macAddressMap)
+            : socket_(std::move(socket)), macAddressMap_(macAddressMap)
+        {
+        }
+
+        void start()
+        {
+            doReadHandshake();
+        }
+
+    private:
+        void doReadHandshake()
+        {
+            auto self(shared_from_this());
+            boost::asio::async_read_until(
+                socket_,
+                boost::asio::dynamic_buffer(handshakeBuffer_),
+                '\n',
+                [this, self](boost::system::error_code ec, std::size_t length)
+                {
+                    if (!ec)
+                    {
+                        std::cout<<handshakeBuffer_;
+                        std::string macAddress = handshakeBuffer_.substr(0, length - 1); // -1 to exclude newline character
+                        std::cout<<"macAddress "<<macAddress;
+                        macAddressMap_[macAddress] = &socket_;
+                        doRead();
+                    }
+                    else
+                    {
+                        std::cerr << "Error during handshake: " << ec.message() << std::endl;
+                    }
+                });
+        }
+
+        void doRead()
+        {
+            auto self(shared_from_this());
+            socket_.async_read_some(
+                boost::asio::buffer(readBuffer_),
+                [this, self](boost::system::error_code ec, std::size_t length)
+                {
+                    if (!ec)
+                    {
+                        processReceivedData(length);
+                        doRead();
+                    }
+                    else
+                    {
+                        std::cerr << "Error during read: " << ec.message() << std::endl;
+                    }
+                });
+        }
+
+        void processReceivedData(std::size_t length)
+        {
+            std::string receivedData(readBuffer_.data(), length);
+            // Do something with the received data
+            std::cout<<receivedData;
+            // Example: Splitting the received data into MAC address and message
+            std::size_t delimiterPos = receivedData.find(':');
+            if (delimiterPos != std::string::npos)
+            {
+                std::string macAddress = receivedData.substr(0, delimiterPos);
+                std::string message = receivedData.substr(delimiterPos + 1);
+
+                send(macAddress, message);
             }
+        }
 
-            startAccept();
-        });
-    }
-
-    void handleClient(std::shared_ptr<tcp::socket> socket)
-    {
-        INFO_LOG("New connection");
-
-        socket->async_read_some(boost::asio::buffer(buffer_),
-                                [this, socket](const boost::system::error_code& error, std::size_t bytes_transferred)
-                                {
-            if(!error)
+        void send(const std::string& macAddress, const std::string& message)
+        {
+            auto it = macAddressMap_.find(macAddress);
+            if (it != macAddressMap_.end())
             {
-                handleHandshake(socket, bytes_transferred);
+                tcp::socket* destinationSocket = it->second;
+
+                boost::asio::async_write(
+                    *destinationSocket,
+                    boost::asio::buffer(message + '\n'),
+                    [](boost::system::error_code ec, std::size_t /*length*/)
+                    {
+                        if (ec)
+                        {
+                            std::cerr << "Error during send: " << ec.message() << std::endl;
+                        }
+                    });
             }
             else
             {
-                handleDisconnect(socket);
+                std::cerr << "Unknown MAC address: " << macAddress << std::endl;
             }
-        });
-    }
+        }
 
-    void handleHandshake(std::shared_ptr<tcp::socket> socket, std::size_t length)
-    {
-        std::string message(buffer_.data(), length);
-        DEBUG_LOG("Handshake: {}", message);
-
-        socket->async_read_some(boost::asio::buffer(buffer_),
-                                [this, socket](const boost::system::error_code& error, std::size_t bytes_transferred)
-                                {
-            if(!error)
-            {
-                handleReceivedData(socket, bytes_transferred);
-            }
-            else
-            {
-                WARN_LOG("Error during handshake: {}", error.message());
-                handleDisconnect(socket);
-            }
-        });
-    }
-
-    void handleReceivedData(std::shared_ptr<tcp::socket> socket, std::size_t length)
-    {
-        std::string message(buffer_.data(), length);
-        DEBUG_LOG("Message recived: {}", message);
-
-        // Przetwarzaj odebrane dane
-
-        // Odpowiedz do klienta (opcjonalnie)
-        // std::string response = "Odpowiedź serwera";
-        // socket->async_write_some(boost::asio::buffer(response),
-        //     [this, socket](const boost::system::error_code& error, std::size_t bytes_transferred) {
-        //         if (!error) {
-        //             // Obsłużono wysłanie odpowiedzi
-        //         } else {
-        //             handleDisconnect(socket);
-        //         }
-        //     });
-
-        // Kontynuuj odbieranie danych od klienta
-        socket->async_read_some(boost::asio::buffer(buffer_),
-                                [this, socket](const boost::system::error_code& error, std::size_t bytes_transferred)
-                                {
-            if(!error)
-            {
-                handleReceivedData(socket, bytes_transferred);
-            }
-            else
-            {
-                WARN_LOG("Error during receive data: {}", error.message());
-                handleDisconnect(socket);
-            }
-        });
-    }
-
-    void handleDisconnect(std::shared_ptr<tcp::socket> socket)
-    {
-        INFO_LOG("The client was disconnected.");
-    }
+        tcp::socket socket_;
+        std::string handshakeBuffer_;
+        std::array<char, 1024> readBuffer_;
+        std::unordered_map<std::string, tcp::socket*>& macAddressMap_;
+    };
 
     tcp::acceptor acceptor_;
-    std::array<char, 1024> buffer_;
+    std::unordered_map<std::string, tcp::socket*> macAddressMap_;
 };
 
 int main()
 {
-    boost::asio::io_context io_context;
-    unsigned short port = 6666;
-
-    TCPServer server(io_context, port);
-
-    io_context.run();
+    try
+    {
+        boost::asio::io_context io_context;
+        TcpServer server(io_context, 6666);
+        io_context.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
     return 0;
 }
