f4bad3246a42adf885e0284c667e48025b420935
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-07-18 20:22:55 +0200

    added zmq lib for conan

--- services/relay/src/Relay.cc
+++ services/relay/src/Relay.cc
@@ -5,7 +5,8 @@
 #include "toad/communication_protocol/tcp/requester/Requester.hh"
 
 int main()
-{
+{ 
+    spdlog::set_level(spdlog::level::trace); // and this
     toad::communication_protocol::tcp::Hub hub;
     toad::communication_protocol::tcp::Requester requester(hub);
     auto endpoint = toad::communication_protocol::Endpoint("0.0.0.0", 5570);
