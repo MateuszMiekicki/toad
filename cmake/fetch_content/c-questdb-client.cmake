8e07dba002302448b9e71eb5a1da071897bd7ec5
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-08-08 20:48:34 +0200

    added exclude questdb targets

--- cmake/fetch_content/c-questdb-client.cmake
+++ cmake/fetch_content/c-questdb-client.cmake
@@ -3,4 +3,8 @@ FetchContent_Declare(
   questdb
   GIT_REPOSITORY https://github.com/questdb/c-questdb-client.git
   GIT_TAG 2.1.3)
-FetchContent_MakeAvailable(questdb)
+FetchContent_GetProperties(questdb)
+if(NOT questdb_POPULATED)
+  FetchContent_Populate(questdb)
+  add_subdirectory(${questdb_SOURCE_DIR} ${questdb_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
